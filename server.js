a6a021c35178d538c286920af2963cd30c8c0cf2
sn6789 <30774592+sn6789@users.noreply.github.com>
2017-08-08 08:50:30 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
